2fc140eb61e232c5b1699f1ba6d33c473250b39e
dayanuyim <dayanuyim@gmail.com>
2020-03-24 20:07:42 +0800

    npm upgrade

--- webpack.config.js
+++ webpack.config.js
@@ -7,47 +7,47 @@ const CopyWebpackPlugin = require('copy-webpack-plugin')
 const webpack = require('webpack');
 
 module.exports = {
-  mode: 'development',
-  entry: './app/index.ts',
-  output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-  },
-  resolve: {
-      extensions: [".ts", ".tsx", ".js"]
-  },
-  plugins: [
-      new HtmlWebpackPlugin({
-          title: 'Trekkr',
-          favicon: 'app/images/favicon.png',
-      }),
-      new webpack.ProvidePlugin({
-          $: 'jquery',
-          jQuery: 'jquery',
-          Popper: 'popper.js'
-      }),
-      new MiniCssExtractPlugin({
-          // Options similar to the same options in webpackOptions.output
-          // both options are optional
-          filename: "[name].css",
-          chunkFilename: "[id].css"
-      }),
-      new CopyWebpackPlugin([
-          {from: 'app/data', to: 'data'},
-          {from: 'app/images', to: 'images'},
-      ]),
-  ],
-  module: {
-      rules: [{
-          test: /\.tsx?$/,
-          loader: 'ts-loader',
-      }, {
-          test: /\.(sa|sc|c)ss$/,
-          use: [ MiniCssExtractPlugin.loader, 'css-loader',/* 'postcss-loader', 'sass-loader', */],
-      }, {
-          test: /\.(png|woff|woff2|eot|ttf|svg)$/,
-          loader: 'url-loader?limit=100000',
-      }],
-  },
+    mode: 'development',
+    entry: './app/index.ts',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Trekkr',
+            favicon: 'app/images/favicon.png',
+        }),
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+            Popper: 'popper.js'
+        }),
+        new MiniCssExtractPlugin({
+            // Options similar to the same options in webpackOptions.output
+            // both options are optional
+            filename: "[name].css",
+            chunkFilename: "[id].css"
+        }),
+        new CopyWebpackPlugin([
+            { from: 'app/data', to: 'data' },
+            { from: 'app/images', to: 'images' },
+        ]),
+    ],
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            loader: 'ts-loader',
+        }, {
+            test: /\.(sa|sc|c)ss$/,
+            use: [MiniCssExtractPlugin.loader, 'css-loader',/* 'postcss-loader', 'sass-loader', */],
+        }, {
+            test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+            loader: 'url-loader?limit=100000',
+        }],
+    },
 };
 
