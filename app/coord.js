f98450704e008f258d52c03727d8a122dfcc9d6c
dayanuyim <dayanuyim@gmail.com>
2020-04-03 16:39:48 +0800

    Hack into Graticule to able to Specify Projection in the Option
    Almost keep OL lib untouched, except of ol/geom/flat/geodesic,
    for exporting the function under it.
    
    steps:
    1. extends Graticule
    2. add the option 'projection' and the property 'gridProjection'
    3. rewrite all functions which used 'EPSG:4326'
    
      - updateProjectionInfo_
         - projection.getWorldExtent => gridProjection.getExtent
         - transform functions
    
      - createGraticule_
         - transformExtent
    
      - getMeridian_
      - getParallel_
         - not use ol/geom/flat/geodesic meridian, which used 'EPSG:4326'
         - not use ol/geom/flat/geodesic parallel, which used 'EPSG:4326'
         - ** need to EXPORT the 'line' function under ol/geom/flat/geodesic  **

--- app/coord.js
+++ app/coord.js
@@ -5,10 +5,10 @@ import proj4 from 'proj4';
 // ref: https://epsg.io/<NUMBER>.js
 // ref: http://mutolisp.logdown.com/posts/207563-taiwan-geodetic-coordinate-system-conversion
  proj4.defs([
-   ["EPSG:3825", "+title=二度分帶：TWD97 TM2 澎湖 +proj=tmerc +lat_0=0 +lon_0=119 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=公尺 +no_defs"],
-   ["EPSG:3826", "+title=二度分帶：TWD97 TM2 台灣 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=公尺 +no_defs"],
-   ["EPSG:3827", "+title=二度分帶：TWD67 TM2 澎湖 +proj=tmerc +lat_0=0 +lon_0=119 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA +towgs84=-752,-358,-179,-0.0000011698,0.0000018398,0.0000009822,0.00002329 +units=公尺"],
-   ["EPSG:3828", "+title=二度分帶：TWD67 TM2 台灣 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA +towgs84=-752,-358,-179,-0.0000011698,0.0000018398,0.0000009822,0.00002329 +units=公尺 +no_defs"],
+   ["EPSG:3825", "+title=二度分帶：TWD97 TM2 澎湖 +proj=tmerc +lat_0=0 +lon_0=119 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=m +no_defs"],
+   ["EPSG:3826", "+title=二度分帶：TWD97 TM2 台灣 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=GRS80 +units=m +no_defs"],
+   ["EPSG:3827", "+title=二度分帶：TWD67 TM2 澎湖 +proj=tmerc +lat_0=0 +lon_0=119 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA +towgs84=-752,-358,-179,-0.0000011698,0.0000018398,0.0000009822,0.00002329 +units=m"],
+   ["EPSG:3828", "+title=二度分帶：TWD67 TM2 台灣 +proj=tmerc +lat_0=0 +lon_0=121 +k=0.9999 +x_0=250000 +y_0=0 +ellps=aust_SA +towgs84=-752,-358,-179,-0.0000011698,0.0000018398,0.0000009822,0.00002329 +units=m +no_defs"],
    //["EPSG:3857", "+title=Web Mercator +proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +wktext  +no_defs"],
    //["EPSG:4326", '+title=WGS 84 (long/lat) +proj=longlat +ellps=WGS84 +datum=WGS84 +units=degrees'],
    //["EPSG:900913", "+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +towgs84=0,0,0,0,0,0,0 +units=m +nadgrids=@null +wktext  +no_defs"],
@@ -28,15 +28,22 @@ register(proj4);
 
 export const TWD67 = new Projection({
   code: 'EPSG:3828',
-  extent: [ 145616.57, 2419514.81, 356704.34, 2803869.61 ]
+  //center: 252551.25 2611288.37
+  extent: [ 145616.57, 2419514.81, 356704.34, 2803869.61 ],
+  worldExtent: [119.99, 21.87, 122.06, 25.34 ],
+  units: 'm',
 });
 
 export const TWD97 = new Projection({
   code: 'EPSG:3826',
-  extent: [-461216.18, 1932367.55, 509174.11, 2985577.33]
+  //center: 40087.26 2452678.85
+  extent: [-461216.18, 1932367.55, 509174.11, 2985577.33],
+  worldExtent: [114.32, 17.36, 123.61, 26.96],
+  units: 'm',
 });
 
 export const WGS84 = getProjection('EPSG:4326');
+export const WEB_MERCATOR = getProjection('EPSG:3857');
 
 export const fromTWD67 = getTransform(TWD67, 'EPSG:3857');
 export const fromTWD97 = getTransform(TWD97, 'EPSG:3857');
@@ -44,12 +51,12 @@ export const fromTWD97 = getTransform(TWD97, 'EPSG:3857');
 // @coordinate: [X, Y, Height, Time]
 export const toTWD67 = (coordinate) => {
     return (coordinate.length <= 2)?
-      transform(coordinate, 'EPSG:3857', TWD67):
-      transform(coordinate.slice(0, 2), 'EPSG:3857', TWD67).concat(coordinate.slice(2));
+      transform(coordinate, WEB_MERCATOR, TWD67):
+      transform(coordinate.slice(0, 2), WEB_MERCATOR, TWD67).concat(coordinate.slice(2));
 }
 
 export const toTWD97 = (coordinate) => {
     return (coordinate.length <= 2)?
-      transform(coordinate, 'EPSG:3857', TWD97):
-      transform(coordinate.slice(0, 2), 'EPSG:3857', TWD97).concat(coordinate.slice(2));
+      transform(coordinate, WEB_MERCATOR, TWD97):
+      transform(coordinate.slice(0, 2), WEB_MERCATOR, TWD97).concat(coordinate.slice(2));
 }
